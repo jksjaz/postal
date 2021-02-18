964d17ca3780b782489adbc21cda81ebc6a119ba
Jaskaran Singh <44025944+jksjaz@users.noreply.github.com>
2021-02-18 01:23:07 -0600

    Update ubuntu1804.sh

--- script/install/ubuntu1804.sh
+++ script/install/ubuntu1804.sh
@@ -24,8 +24,27 @@ apt install -y software-properties-common
 apt-add-repository ppa:brightbox/ruby-ng -y
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 add-apt-repository 'deb [arch=amd64] http://mirrors.coreix.net/mariadb/repo/10.1/ubuntu bionic main'
+
+## Install prerequisites
+sudo apt-get install curl gnupg -y
+
+## Install RabbitMQ signing key
 curl -fsSL https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc | sudo apt-key add -
-add-apt-repository 'deb http://www.rabbitmq.com/debian/ bionic main'
+
+## Install apt HTTPS transport
+sudo apt-get install apt-transport-https
+
+## Add Bintray repositories that provision latest RabbitMQ and Erlang 23.x releases
+sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list <<EOF
+## Installs the latest Erlang 23.x release.
+## Change component to "erlang-22.x" to install the latest 22.x version.
+## "bionic" as distribution name should work for any later Ubuntu or Debian release.
+## See the release to distribution mapping table in RabbitMQ doc guides to learn more.
+deb https://dl.bintray.com/rabbitmq-erlang/debian bionic erlang
+## Installs latest RabbitMQ release
+deb https://dl.bintray.com/rabbitmq/debian bionic main
+EOF
+
 apt update
 export DEBIAN_FRONTEND=noninteractive
 apt install -y ruby2.3 ruby2.3-dev build-essential libssl-dev mariadb-server libmysqlclient-dev rabbitmq-server nodejs git nginx wget nano
